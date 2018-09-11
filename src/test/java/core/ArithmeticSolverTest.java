615848e015902216614ffbc4dee1169b84080783
Vin Abs <vineetabrol54@gmail.com>
2018-09-10 23:29:59 -0400

    forgot to save test file

--- src/test/java/core/ArithmeticSolverTest.java
+++ src/test/java/core/ArithmeticSolverTest.java
@@ -1,5 +1,38 @@
 package core;
 
-public class ArithmeticSolverTest {
+import junit.framework.TestCase;
 
+public class ArithmeticSolverTest extends TestCase {
+
+	public void testAdd() {
+		ArithmeticSolver solver = new AritmeticSolver();
+		
+		assertEquals(4, solver.add(2,2));
+	}
+	public void testSubtract() {
+ArithmeticSolver solver = new AritmeticSolver();
+		
+		assertEquals(2, solver.subtract(4,2));
+		
+	}
+	public void testMulitply() {
+ArithmeticSolver solver = new AritmeticSolver();
+		
+		assertEquals(4, solver.multiply(2,2));
+		
+	}
+	public void testDivide() {
+ArithmeticSolver solver = new AritmeticSolver();
+		
+		assertEquals(1, solver.divide(2,2));
+		
+	}
+	
+	public void testModulus() {
+ArithmeticSolver solver = new AritmeticSolver();
+		
+		assertEquals(1, solver.modulus(5,2));
+		
+	}
 }
+
