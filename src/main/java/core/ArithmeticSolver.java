cc43f59fa00363f427911fb0d759561461ba1a07
Vin Abs <vineetabrol54@gmail.com>
2018-09-10 23:38:29 -0400

    made implementation file in main folder

---
+++ src/main/java/core/ArithmeticSolver.java
@@ -0,0 +1,24 @@
+package core;
+
+public class ArithmeticSolver {
+
+	public double add(double num1, double num2) {
+		return num1 + num2;
+		
+	}
+	
+	public double subtract(double num1, double num2) {
+		return num1 - num2;
+	}
+	
+	public double multiply(double num1, double num2) {
+		return num1 * num2;
+	}
+	public double divide(double num1, double num2) {
+		return num1/num2;
+	}
+	public double modulus(double num1, double num2) {
+		return num1%num2;
+	}
+}
+
--- src/test/java/core/ArithmeticSolverTest.java
+++ src/test/java/core/ArithmeticSolverTest.java
@@ -5,31 +5,31 @@ import junit.framework.TestCase;
 public class ArithmeticSolverTest extends TestCase {
 
 	public void testAdd() {
-		ArithmeticSolver solver = new AritmeticSolver();
+		ArithmeticSolver solver = new ArithmeticSolver();
 		
 		assertEquals(4, solver.add(2,2));
 	}
 	public void testSubtract() {
-ArithmeticSolver solver = new AritmeticSolver();
+ArithmeticSolver solver = new ArithmeticSolver();
 		
 		assertEquals(2, solver.subtract(4,2));
 		
 	}
 	public void testMulitply() {
-ArithmeticSolver solver = new AritmeticSolver();
+ArithmeticSolver solver = new ArithmeticSolver();
 		
 		assertEquals(4, solver.multiply(2,2));
 		
 	}
 	public void testDivide() {
-ArithmeticSolver solver = new AritmeticSolver();
+ArithmeticSolver solver = new ArithmeticSolver();
 		
 		assertEquals(1, solver.divide(2,2));
 		
 	}
 	
 	public void testModulus() {
-ArithmeticSolver solver = new AritmeticSolver();
+ArithmeticSolver solver = new ArithmeticSolver();
 		
 		assertEquals(1, solver.modulus(5,2));
 		
