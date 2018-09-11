7e154b8ef4285417cf72d9dd932cff6817544adf
Vin Abs <vineetabrol54@gmail.com>
2018-09-10 23:46:09 -0400

    fixed fails

--- src/test/java/core/ArithmeticSolverTest.java
+++ src/test/java/core/ArithmeticSolverTest.java
@@ -7,31 +7,31 @@ public class ArithmeticSolverTest extends TestCase {
 	public void testAdd() {
 		ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(4, solver.add(2,2));
+		assertEquals(4.0, solver.add(2,2));
 	}
 	public void testSubtract() {
 ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(2, solver.subtract(4,2));
+		assertEquals(2.0, solver.subtract(4,2));
 		
 	}
 	public void testMulitply() {
 ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(4, solver.multiply(2,2));
+		assertEquals(4.0, solver.multiply(2,2));
 		
 	}
 	public void testDivide() {
 ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(1, solver.divide(2,2));
+		assertEquals(1.0, solver.divide(2,2));
 		
 	}
 	
 	public void testModulus() {
 ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(1, solver.modulus(5,2));
+		assertEquals(1.0, solver.modulus(5,2));
 		
 	}
 }
